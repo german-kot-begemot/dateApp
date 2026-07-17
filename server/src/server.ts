c25cd02d17ffc546e4830e9a3b58799f6ccceed5
Elena Demina <lena.demina@hotmail.com>
2026-07-17 15:19:31 +0200

    chore: Prepare app for production deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,23 +4,31 @@ import dotenv from 'dotenv';
 import apiRoutes from './routes/api.js';
 import { connectDB } from './db.js';
 import { setTelegramWebhook } from './services/telegramWebhook.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/api', apiRoutes);
+
+const clientBuildPath = path.join(__dirname, '../../client/dist');
+
+app.use(express.static(clientBuildPath));
+app.get(/^(?!\/api).*/, (_, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
 
 const start = async () => {
   try {
-    await Promise.all([
-      connectDB(),
-      setTelegramWebhook(),
-    ]);
+    await Promise.all([connectDB(), setTelegramWebhook()]);
 
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
